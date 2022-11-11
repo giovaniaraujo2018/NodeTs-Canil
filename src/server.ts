47bd84e7a45edc5aa4eff3820c4aed393c6aebac
Giovani Mendes <giovaniaraujo@hotmail.com>
2022-11-11 11:43:02 -0300

    heroku setup

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,14 @@ import mustache from 'mustache-express';
 import path from 'path';
 import mainRoutes from './routes/index';
 
+
+
 dotenv.config();
 
 const server = express();
 
 server.set('view engine', 'mustache');
-server.set('views', path.join(__dirname, './views'));
+server.set('views', path.join(__dirname,'views'));
 server.engine('mustache', mustache());
 
 server.use(express.static(path.join(__dirname, '../public')));
